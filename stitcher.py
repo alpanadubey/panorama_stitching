8c264457dd530c0430f1a705ba8eb9b0115e05a8
alpanadubey <alpanadubey975@gmail.com>
2025-05-08 11:25:18 +0530

    Update stitcher.py

--- stitcher.py
+++ stitcher.py
@@ -31,17 +31,40 @@ def stitch_images(img1, img2, descriptor_type='sift'):
     if len(good_matches) < 4:
         raise Exception("Not enough matches")
 
-    src_pts = np.float32([kp1[m.queryIdx].pt for m in good_matches]).reshape(-1,1,2)
-    dst_pts = np.float32([kp2[m.trainIdx].pt for m in good_matches]).reshape(-1,1,2)
+    src_pts = np.float32([kp1[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+    dst_pts = np.float32([kp2[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
 
     H, _ = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
-    stitched = cv2.warpPerspective(img1, H, (img2.shape[1] + img1.shape[1], img2.shape[0]))
-    stitched[0:img2.shape[0], 0:img2.shape[1]] = img2
+
+    # Warp img1 to img2's perspective
+    height1, width1 = img1.shape[:2]
+    height2, width2 = img2.shape[:2]
+
+    # Find corners of the warped image
+    corners_img1 = np.float32([[0, 0], [0, height1], [width1, height1], [width1, 0]]).reshape(-1, 1, 2)
+    corners_img1_transformed = cv2.perspectiveTransform(corners_img1, H)
+
+    corners_img2 = np.float32([[0, 0], [0, height2], [width2, height2], [width2, 0]]).reshape(-1, 1, 2)
+
+    all_corners = np.concatenate((corners_img1_transformed, corners_img2), axis=0)
+    [xmin, ymin] = np.int32(all_corners.min(axis=0).ravel() - 0.5)
+    [xmax, ymax] = np.int32(all_corners.max(axis=0).ravel() + 0.5)
+
+    translation = [-xmin, -ymin]
+
+    # Translate homography to shift image into positive space
+    translation_mat = np.array([[1, 0, translation[0]], [0, 1, translation[1]], [0, 0, 1]])
+    H_translated = translation_mat @ H
+
+    # Warp first image
+    result = cv2.warpPerspective(img1, H_translated, (xmax - xmin, ymax - ymin))
+
+    # Paste second image into the result
+    result[translation[1]:translation[1] + height2, translation[0]:translation[0] + width2] = img2
 
     cv2.imwrite('static/matched.jpg', match_img)
-    stitched = crop_black(stitched)
 
-    return stitched
+    return crop_black(result)
 
 def crop_black(image):
     gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
